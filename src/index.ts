fe2e1a8beec03c32ef3865011a8bafd6f67031b0
mhaligowski <mhaligowski@gmail.com>
2021-11-10 19:21:57 -0800

    Draw strings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import { SVG } from "@svgdotjs/svg.js";
 
-var draw = SVG().addTo("#root");
-var rect = draw.rect(100, 100).attr({ fill: "#f06" });
+const VERTICAL_SPACE: number = 20;
+const STRING_COUNT: number = 6;
+
+var draw = SVG().addTo("#root").size(300, 100);
+
+// Draw vertical line
+draw
+  .line(0, 0, 0, STRING_COUNT * VERTICAL_SPACE)
+  .stroke({ color: "#000", width: 10 });
+
+// Draw strings
+for (var i = 0; i < STRING_COUNT; i++) {
+  const top = VERTICAL_SPACE * i;
+  draw.line(0, top, 100, top).stroke({ color: "#000", width: 1 });
+}
