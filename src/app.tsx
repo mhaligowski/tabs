e0b8b7d84deaa76e4f64da8a8c9ebabffaf5ddab
mhaligowski <mhaligowski@gmail.com>
2022-02-07 10:54:06 -0800

    👤 Install Firebase.js

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,24 @@ import { useSelector } from "react-redux";
 
 import { ShareWidget } from "./share/widget";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAApm4gMMg7DGIpM6kZH98Xx42oFLH9O-8",
+  authDomain: "tabs-331912.firebaseapp.com",
+  projectId: "tabs-331912",
+  storageBucket: "tabs-331912.appspot.com",
+  messagingSenderId: "13558635774",
+  appId: "1:13558635774:web:21f18b0d19d09abbd6d9af",
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app);
+
 const App = () => {
   const tabulature = useSelector((state: any) => state.tabulature);
 
